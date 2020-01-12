bc80b017fac69f5aac36b3c233b3559714e51fa9
mamba2410 <callummcgregor54@gmail.com>
2020-01-12 20:56:30 +0000

    Changed memory addresses to be cast as pointers

--- include/mmio/mailbox.h
+++ include/mmio/mailbox.h
@@ -23,13 +23,13 @@ extern volatile uint32_t mailbox[36];
 #define MBOX_TAG_LAST		0
 
 
-#define VIDEOCORE_MBOX  (MMIO_BASE+0x0000B880)
-#define MBOX_READ       (VIDEOCORE_MBOX+0x00)
-#define MBOX_POLL       (VIDEOCORE_MBOX+0x10)
-#define MBOX_SENDER     (VIDEOCORE_MBOX+0x14)
-#define MBOX_STATUS     (VIDEOCORE_MBOX+0x18)
-#define MBOX_CONFIG     (VIDEOCORE_MBOX+0x1C)
-#define MBOX_WRITE      (VIDEOCORE_MBOX+0x20)
+#define VIDEOCORE_MBOX  MMIO_BASE+0x0000B880
+#define MBOX_READ       ((uint64_t*)(VIDEOCORE_MBOX+0x00))
+#define MBOX_POLL       ((uint64_t*)(VIDEOCORE_MBOX+0x10))
+#define MBOX_SENDER     ((uint64_t*)(VIDEOCORE_MBOX+0x14))
+#define MBOX_STATUS     ((uint64_t*)(VIDEOCORE_MBOX+0x18))
+#define MBOX_CONFIG     ((uint64_t*)(VIDEOCORE_MBOX+0x1C))
+#define MBOX_WRITE      ((uint64_t*)(VIDEOCORE_MBOX+0x20))
 #define MBOX_RESPONSE   0x80000000
 #define MBOX_FULL       0x80000000
 #define MBOX_EMPTY      0x40000000
