5d1cf644ab98553d934b8a9581902a8e08261588
mamba2410 <callummcgregor54@gmail.com>
2020-01-14 12:56:45 +0000

    Fixed baud rate for mini uart

--- include/mmio/gpio.h
+++ include/mmio/gpio.h
@@ -26,6 +26,7 @@
 
 #define SYS_CLOCK_FREQ		250e6									// System clock frequency is 250MHz
 #define BAUD_RATE			115200									// Chosen baud rate
-#define BAUD_RATE_REG_VAL	(SYS_CLOCK_FREQ/(8 * BAUD_RATE) - 1)	// Calculate the register value for the baud rate
+//#define BAUD_RATE_REG_VAL	(SYS_CLOCK_FREQ/(8 * BAUD_RATE) - 1)	// Calculate the register value for the baud rate
+#define BAUD_RATE_REG_VAL	270
 
 #endif
